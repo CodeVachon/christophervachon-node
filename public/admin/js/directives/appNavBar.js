22380c32e527b3bfe223177b892f2fee9113e84f
Christopher Vachon <code@christophervachon.com>
2015-03-02 12:05:53 -0500

    Angular - Added AppNavBar Directive

---
+++ public/admin/js/directives/appNavBar.js
@@ -0,0 +1,29 @@
+angular.module('Administrator')
+.directive('appNavBar',["$sce", "$window", "$location",
+function($sce, $window, $location) {
+    return {
+        replace: true,
+        restrict: "E",
+        templateUrl: './templates/directives/appNavBar.html',
+        controller: function($scope) {
+            $scope.isLoggedIn = function() {
+                return $window.sessionStorage.token;
+            }
+            $scope.getActive = function()  {
+                var value = $location.path().replace(/^\/([a-zA-Z0-9]+)(?:\/.+)$/gi,"$1").replace(/\//g,"").toLowerCase();
+                return value;
+            }
+            $scope.isActiveMenu = function(value) {
+                var currentValue = $scope.getActive();
+                return (currentValue === value);
+            }
+            $scope.getActiveUserId = function() {
+                var userData = JSON.parse($window.sessionStorage.user);
+                return userData._id;
+            }
+        },
+        link: function() {
+
+        }
+    };
+}]);
