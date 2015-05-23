8b37facbb9cf5abbca82a36d8fccadf0dd9434cb
Christopher Vachon <code@christophervachon.com>
2015-05-23 19:59:31 -0400

    Update the Homepage with Article and Twitter Feed

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ var express = require('express'),
     MongoURL = process.env.MONGO_URL || "mongodb://localhost/"+dbName,
 
     fs = require('fs'),
-    _siteSettings = {defaults: {keywords:[]}}
+    _siteSettings = {defaults: {keywords:[]}},
+
+    Twitter = require('twitter'),
+    client = new Twitter({
+        consumer_key: process.env.twitter_consumer_key || "uMdauo5lw43JRap1sRjY4ESBx",
+        consumer_secret: process.env.twitter_consumer_secret || "79p2gvOuhDD6SioiFycLQSyMYQwIDrL8J0YbDPC2Z7jundM8rS",
+        access_token_key: process.env.twitter_access_token_key || "18299192-zBBxEfw1XKXz4JI47kCwnKx2aZ0UA7JwOMgkx1tIt",
+        access_token_secret: process.env.twitter_access_token_secret|| "d21YpMh1kFKmfTjjTYrNaswpJWgyLL0nAcUsoCxSqW9tV",
+    }),
+    Posts = require("./models/article"),
+    md5 = require('MD5'),
+    Showdown = require("./public/admin/js/vendor/showdown"),
+    converter = new Showdown.converter({ extensions: ['twitter'] })
 ;
 
 if (process.env.testing) {  console.log("APPLICATION IS IN TESTING MODE!!!");  }
@@ -44,28 +56,28 @@ app.set("googleAnalytics",  _siteSettings.googleAnalytics || "" );
 
 // This Creates a Request Scope that can be used inside of the Jade Views
 app.use(function(request, response, next) {
-	var _hostPath = "";
-	var _canonical = [];
-	_canonical.push(request.protocol + "://");
-	if (request.subdomains.length > 0) {
-		_canonical.push(request.subdomains.join(".") + ".");
-	}
-	_canonical.push(request.hostname);
-	_hostPath = _canonical.join("").toLowerCase();
-	_canonical.push(request.originalUrl);
+    var _hostPath = "";
+    var _canonical = [];
+    _canonical.push(request.protocol + "://");
+    if (request.subdomains.length > 0) {
+        _canonical.push(request.subdomains.join(".") + ".");
+    }
+    _canonical.push(request.hostname);
+    _hostPath = _canonical.join("").toLowerCase();
+    _canonical.push(request.originalUrl);
 
-	response.locals.request = {
-		path: request.path,
-		protocol: request.protocol,
-		query: request.query,
-		originalUrl: request.originalUrl,
-		hostname: request.hostname,
-		host: _hostPath,
-		subdomains: request.subdomains.join("."),
-		canonical: _canonical.join("").toLowerCase(),
+    response.locals.request = {
+        path: request.path,
+        protocol: request.protocol,
+        query: request.query,
+        originalUrl: request.originalUrl,
+        hostname: request.hostname,
+        host: _hostPath,
+        subdomains: request.subdomains.join("."),
+        canonical: _canonical.join("").toLowerCase(),
         OGImage: _hostPath + _siteSettings.defaults.OGImage
-	};
-	next();
+    };
+    next();
 });
 
 
@@ -74,7 +86,63 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (request, response) {
-    response.render('index', {});
+
+    var _endDate = new Date();
+    Posts.find({
+        "publish_date": {
+            $lt: _endDate
+        }
+    }, null, {limit: 10, sort: {publish_date: -1}},function (error, posts) {
+        if (error) { console.log(error); }
+        //console.log(posts.length);
+        var params = {screen_name: 'liaodrake', count: 10};
+        client.get('statuses/user_timeline', params, function(error, tweets, t_response){
+            if (error) { console.log(error); }
+            //console.log(tweets.length);
+
+            var RAW_activityfeed = {};
+            for (var i=0,x=posts.length; i<x; i++) {
+                RAW_activityfeed[ +new Date(posts[i].publish_date) ] = { 
+                    type: "post",
+                    data: posts[i]
+                };
+            }
+            for (var i=0,x=tweets.length; i<x; i++) {
+                RAW_activityfeed[ +new Date(tweets[i].created_at) ] = { 
+                    type: "tweet",
+                    data: tweets[i]
+                };
+                //console.log("---");
+                //console.log(tweets[i].entities);
+            }
+            var RAW_activityfeed_timestamps = Object.keys(RAW_activityfeed);
+            RAW_activityfeed_timestamps.sort().reverse();
+
+            //console.log(RAW_activityfeed_timestamps);
+
+            var activityfeed = [];
+            for (var i=0,x=10; i<x; i++) {
+                activityfeed.push(RAW_activityfeed[RAW_activityfeed_timestamps[i]]);
+            }
+
+            //console.log(activityfeed);
+
+            response.render('index', {
+                activityfeed: activityfeed,
+                utl:utl,
+                md5:md5,
+                converter:converter,
+                socialMedia: [
+                    {
+                        label:"Twitter",
+                        url: "https://twitter.com/liaodrake",
+                        imgsrc: "https://g.twimg.com/dev/documentation/image/Twitter_logo_blue_48.png",
+                        username: "liaodrake"
+                    }
+                ]
+            });
+        }); // close client
+    }); // close Posts.find
 }); // close get('/')
 
 // Handel Authentication Tokens
